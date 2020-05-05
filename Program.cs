ac29d3fe969f3440f2feee8cbffb03b5e9442c3c
scottbisese <bisese.scott@gmail.com>
2020-05-05 17:34:13 -0500

    added sln and console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LAB02_ATM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
